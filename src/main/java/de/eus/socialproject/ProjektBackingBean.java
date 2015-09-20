64bb58d31968086c7bb36557f217168b58a97c62
Emmi Luise <Emmi10005@gmail.com>
2015-09-20 11:12:50 +0200

    funktionierendes Projekt

--- src/main/java/de/eus/socialproject/ProjektBackingBean.java
+++ src/main/java/de/eus/socialproject/ProjektBackingBean.java
@@ -4,31 +4,80 @@
  * and open the template in the editor.
  */
 package de.eus.socialproject;
+
 import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
+
 /**
  *
  * @author Emmi
  */
-@Named 
-@SessionScoped 
+@Named
+@SessionScoped
 
 public class ProjektBackingBean implements Serializable {
+
     private String email;
+    private String name;
+    private String projektname;
+    private String projektidee;
+    private String ort;
+    private String zeitraum;
+
+    public String getName() {
+        return name;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProjektname() {
+        return projektname;
+    }
+
+    public void setProjektname(String projektname) {
+        this.projektname = projektname;
+    }
+
+    public String getProjektidee() {
+        return projektidee;
+    }
+
+    public void setProjektidee(String projektidee) {
+        
+        this.projektidee = projektidee;
+    }
+
+    public String getOrt() {
+        return ort;
+    }
+
+    public void setOrt(String ort) {
+        this.ort = ort;
+    }
+
+    public String getZeitraum() {
+        return zeitraum;
+    }
+
+    public void setZeitraum(String zeitraum) {
+        this.zeitraum = zeitraum;
+    }
 
     public String getEmail() {
         return email;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public String submit() {
+        System.out.println(email);
+        setEmail(null);
+        return "Emmi";
+
     }
-     public String submit(){
-     System.out.println(email);
-     setEmail(null);
-     return "Emmi";
-    
- }  
 }
